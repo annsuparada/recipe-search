b385bb9e916fc50f4832a1985b8f35484194ebab
Ann <ann.suparada@gmail.com>
2020-10-30 11:18:41 -0500

    sort popularity

--- client/src/store/actions/advanceSearchAction.js
+++ client/src/store/actions/advanceSearchAction.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios from "axios";
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 const BASE_URL = "https://api.spoonacular.com";
@@ -9,42 +9,54 @@ export const FETCH_ADVANCE_SEARCH_FAILURE = "FETCH_ADVANCE_SEARCH_FAILURE";
 export const TOGGLE_ADVANCE_SEARCH = "TOGGLE_ADVANCE_SEARCH";
 const listCount = 20;
 
-export const toggleAdvanceSearch = (boolean) => dispatch => {
-    dispatch({ type: TOGGLE_ADVANCE_SEARCH, payload: boolean })
-}
+export const toggleAdvanceSearch = (boolean) => (dispatch) => {
+  dispatch({ type: TOGGLE_ADVANCE_SEARCH, payload: boolean });
+};
 
-export const getAdvanceRecipes = (recipe, minCalories, maxCalories,
-    minCarbs, maxCarbs, minProtein, maxProtein, minFat, maxFat, diet, intolerances) => async (dispatch) => {
+export const getAdvanceRecipes = (
+  recipe,
+  minCalories,
+  maxCalories,
+  minCarbs,
+  maxCarbs,
+  minProtein,
+  maxProtein,
+  minFat,
+  maxFat,
+  diet,
+  intolerances
+) => async (dispatch) => {
+  dispatch({ type: FETCH_ADVANCE_SEARCH_START });
 
-        dispatch({ type: FETCH_ADVANCE_SEARCH_START })
-
-        const localStorageKey = `/recipes/complexSearch?query=${recipe}&number=${listCount}&addRecipeNutrition=true
+  const localStorageKey = `/recipes/complexSearch?query=${recipe}&number=${listCount}&addRecipeNutrition=true
         &minCalories=${minCalories}&maxCalories=${maxCalories}&minCarbs=${minCarbs}&maxCarbs=${maxCarbs}&minProtein=${minProtein}
-        &maxProtein=${maxProtein}&minFat=${minFat}&maxFat=${maxFat}&diet=${diet}&intolerances=${intolerances}&instructionsRequired=true&apiKey=${API_KEY}`;
+        &maxProtein=${maxProtein}&minFat=${minFat}&maxFat=${maxFat}&diet=${diet}&intolerances=${intolerances}&instructionsRequired=true&sort=popularity&apiKey=${API_KEY}`;
 
-        if (localStorage.getItem(localStorageKey)) {
-            console.log('USED LOCAL CACHE');
-            dispatch({
-                type: FETCH_ADVANCE_SEARCH_SUCCESS,
-                payload: JSON.parse(localStorage.getItem(localStorageKey))
-            });
-        } else {
-            axios.get(`${BASE_URL}${localStorageKey}`)
-                .then(response => {
-                    console.log('FETCHED FROM API');
-                    dispatch({
-                        type: FETCH_ADVANCE_SEARCH_SUCCESS,
-                        payload: response.data.results
-                    });
-                    localStorage.setItem(localStorageKey, JSON.stringify(response.data.results));
-
-                })
-                .catch(error => {
-                    dispatch({
-                        type: FETCH_ADVANCE_SEARCH_FAILURE,
-                        payload: error
-                    })
-                })
-        }
-
-    }
+  if (localStorage.getItem(localStorageKey)) {
+    console.log("USED LOCAL CACHE");
+    dispatch({
+      type: FETCH_ADVANCE_SEARCH_SUCCESS,
+      payload: JSON.parse(localStorage.getItem(localStorageKey)),
+    });
+  } else {
+    axios
+      .get(`${BASE_URL}${localStorageKey}`)
+      .then((response) => {
+        console.log("FETCHED FROM API");
+        dispatch({
+          type: FETCH_ADVANCE_SEARCH_SUCCESS,
+          payload: response.data.results,
+        });
+        localStorage.setItem(
+          localStorageKey,
+          JSON.stringify(response.data.results)
+        );
+      })
+      .catch((error) => {
+        dispatch({
+          type: FETCH_ADVANCE_SEARCH_FAILURE,
+          payload: error,
+        });
+      });
+  }
+};
--- client/src/store/actions/recipesAction.js
+++ client/src/store/actions/recipesAction.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios from "axios";
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 const BASE_URL = "https://api.spoonacular.com";
@@ -10,72 +10,72 @@ export const FETCH_RECIPES_FAILURE = "FETCH_RECIPES_FAILURE";
 const listCount = 20;
 
 export const getRecipes = (searchTerm) => async (dispatch) => {
-    dispatch({ type: FETCH_RECIPES_START })
-    const localStorageKey = `/recipes/complexSearch?query=${searchTerm}&instructionsRequired=true&number=${listCount}&addRecipeNutrition=true&apiKey=${API_KEY}`;
+  dispatch({ type: FETCH_RECIPES_START });
+  const localStorageKey = `/recipes/complexSearch?query=${searchTerm}&instructionsRequired=true&number=${listCount}&addRecipeNutrition=true&sort=popularity&apiKey=${API_KEY}`;
 
-    if (localStorage.getItem(localStorageKey)) {
-        console.log('USED LOCAL CACHE');
+  if (localStorage.getItem(localStorageKey)) {
+    console.log("USED LOCAL CACHE");
+    dispatch({
+      type: FETCH_RECIPES_SUCCESS,
+      payload: JSON.parse(localStorage.getItem(localStorageKey)),
+    });
+  } else {
+    axios
+      .get(`${BASE_URL}${localStorageKey}`)
+      .then((response) => {
+        console.log("FETCHED FROM API");
         dispatch({
-            type: FETCH_RECIPES_SUCCESS,
-            payload: JSON.parse(localStorage.getItem(localStorageKey))
+          type: FETCH_RECIPES_SUCCESS,
+          payload: response.data.results,
         });
-    } else {
-        axios.get(`${BASE_URL}${localStorageKey}`)
-            .then(response => {
-                console.log('FETCHED FROM API');
-                dispatch({
-                    type: FETCH_RECIPES_SUCCESS,
-                    payload: response.data.results
-                });
-                localStorage.setItem(localStorageKey, JSON.stringify(response.data.results));
-
-            })
-            .catch(error => {
-                dispatch({
-                    type: FETCH_RECIPES_FAILURE,
-                    payload: error
-                })
-            })
-    }
-
-}
-
+        localStorage.setItem(
+          localStorageKey,
+          JSON.stringify(response.data.results)
+        );
+      })
+      .catch((error) => {
+        dispatch({
+          type: FETCH_RECIPES_FAILURE,
+          payload: error,
+        });
+      });
+  }
+};
 
 export const FETCH_RECIPE_BY_ID_START = "FETCH_RECIPE_BY_ID_START";
 export const FETCH_RECIPE_BY_ID_SUCCESS = "FETCH_RECIPE_BY_ID_SUCCESS";
 export const FETCH_RECIPE_BY_ID_FAILURE = "FETCH_RECIPE_BY_ID_FAILURE";
 
-export const getRecipeById = (id) => dispatch => {
+export const getRecipeById = (id) => (dispatch) => {
+  dispatch({ type: FETCH_RECIPE_BY_ID_START });
 
-    dispatch({ type: FETCH_RECIPE_BY_ID_START });
+  const localStorageKey = `recipes/${id}`;
 
-    const localStorageKey = `recipes/${id}`;
-
-    if (localStorage.getItem(localStorageKey)) {
-        console.log('USED LOCAL CACHE');
-        const responses = JSON.parse(localStorage.getItem(localStorageKey));
+  if (localStorage.getItem(localStorageKey)) {
+    console.log("USED LOCAL CACHE");
+    const responses = JSON.parse(localStorage.getItem(localStorageKey));
+    dispatch({
+      type: FETCH_RECIPE_BY_ID_SUCCESS,
+      payload: responses,
+    });
+  } else {
+    return axios
+      .get(
+        `${BASE_URL}/recipes/${id}/information?includeNutrition=true&apiKey=${API_KEY}`
+      )
+      .then((responses) => {
+        console.log("FETCHED FROM API", responses.data);
         dispatch({
-            type: FETCH_RECIPE_BY_ID_SUCCESS,
-            payload: responses
+          type: FETCH_RECIPE_BY_ID_SUCCESS,
+          payload: responses.data,
         });
-    } else {
-        
-        return axios
-        .get(`${BASE_URL}/recipes/${id}/information?includeNutrition=true&apiKey=${API_KEY}`)
-            .then(responses => {
-                console.log('FETCHED FROM API', responses.data);
-                dispatch({
-                    type: FETCH_RECIPE_BY_ID_SUCCESS,
-                    payload: responses.data
-                });
-                localStorage.setItem(localStorageKey, JSON.stringify(responses.data))
-            })
-            .catch(error => {
-                dispatch({
-                    type: FETCH_RECIPE_BY_ID_FAILURE,
-                    payload: error
-                })
-            })
-    }
-
-}
+        localStorage.setItem(localStorageKey, JSON.stringify(responses.data));
+      })
+      .catch((error) => {
+        dispatch({
+          type: FETCH_RECIPE_BY_ID_FAILURE,
+          payload: error,
+        });
+      });
+  }
+};
